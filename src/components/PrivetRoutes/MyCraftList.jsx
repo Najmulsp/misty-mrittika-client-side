9b5b7e48c0385f13b65f1dfaedd92bb6d25ad70d
Najmulsp <mdnajmulsarker1595@gmail.com>
2024-04-27 00:40:59 +0600

    added delete my craft list

--- src/components/PrivetRoutes/MyCraftList.jsx
+++ src/components/PrivetRoutes/MyCraftList.jsx
@@ -1,11 +1,11 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const MyCraftList = () => {
   const { user } = useContext(AuthContext);
   const [myCraft, setMyCraft] = useState([]);
-  
 
   useEffect(() => {
     fetch(`http://localhost:5000/myCraftList/${user?.email}`)
@@ -14,9 +14,39 @@ const MyCraftList = () => {
         setMyCraft(data);
       });
   }, [user]);
+
+  const handleDeleteMyCraft = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/deleteMyCraft/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              const remaining = myCraft.filter((craft) => craft._id !== id);
+              setMyCraft(remaining);
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
   return (
     <div className="w-full grid grid-cols-2 p-10 mx-auto gap-6 border ">
-
       {myCraft.map((craft) => (
         <div key={craft._id}>
           <div className="card w-96 bg-base-100 shadow-xl">
@@ -34,9 +64,14 @@ const MyCraftList = () => {
               <p>{craft.description}</p>
               <div className="card-actions justify-end">
                 <Link to={`/updateMyCraft/${craft._id}`}>
-                <div className="badge badge-outline">Update</div>
+                  <button className="btn btn-primary">Update</button>
                 </Link>
-                <div className="badge badge-outline">Delete</div>
+                <button
+                  onClick={() => handleDeleteMyCraft(craft._id)}
+                  className="btn btn-secondary"
+                >
+                  Delete
+                </button>
               </div>
             </div>
           </div>
